3a79109c1c6d464a51836ecf22e79f319fdf8416
Michael Calvey <mjcalvey2@gmail.com>
2017-02-06 13:14:44 +0000

    Adds nighttime scripts

---
+++ NightTime.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AC.TimeOfDaySystemFree;
+
+public class NightTime : MonoBehaviour {
+
+	public Material Streetlight;
+	public Material Buildinglight;
+	public float time;
+	private GameObject[] Lamps;
+
+	void Start() 
+	{
+		Lamps = GameObject.FindGameObjectsWithTag ("StreetLight");
+	}
+
+	void Update()
+	{
+		time = GameObject.FindGameObjectWithTag ("TimeManager").GetComponent<TimeOfDayManager> ().timeline;
+		if (time <= 5.49f || time>= 18.49f) 
+		{
+			Buildinglight.SetColor ("_EmissionColor", Color.yellow);
+			Streetlight.SetColor ("_EmissionColor", Color.yellow);
+			for(int i = 0; i < Lamps.Length; i++)
+			{
+				Lamps [i].GetComponent<Light>().enabled = true;
+			}
+
+		} 
+		else 
+		{
+			Buildinglight.SetColor ("_EmissionColor", Color.black);
+			Streetlight.SetColor ("_EmissionColor", Color.black);
+			for(int i = 0; i < Lamps.Length; i++)
+			{
+				Lamps [i].GetComponent<Light>().enabled = false;
+			}
+		}
+	}
+}
