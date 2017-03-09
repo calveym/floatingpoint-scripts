119a5c12b2e8e6b405f53f104cb57969ed8054d1
Michael Calvey <mjcalvey2@gmail.com>
2017-03-09 21:34:29 +0000

    Revert "Revert "Progression and saving""
    This reverts commit 0a2a0fc0bb1294ea424ad032b77bd5acca1ba7d0.

---
+++ Managers/SaveManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SaveManager : MonoBehaviour {
+
+    public void Save()
+    {
+        Debug.Log("Saving...");
+        LevelSerializer.SaveGame("test");
+    }
+
+    public void Load ()
+    {
+        Debug.Log(LevelSerializer.SavedGames["test"]);
+    }
+}
