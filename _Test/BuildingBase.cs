d33a17b92d200d568734d398c4ca004c879db18f
Michael Calvey <mjcalvey2@gmail.com>
2017-06-20 13:46:32 +0100

    Starting rewrite of trackers as separate interfaces with proper inheritance

---
+++ _Test/BuildingBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BuildingBase : IPersonContainer {
+    
+    public int capacity { get; set; }
+
+    public int users { get; set; }
+
+
+	public void AddUsers(int numUsers)
+    {
+        if (numUsers + users <= capacity)
+            users += numUsers;
+    }
+
+    public void RemoveUsers(int numUsers)
+    {
+        if (users - numUsers >= 0)
+            users -= numUsers;
+    }
+
+    public bool Full()
+    {
+        if (users == capacity)
+            return true;
+        else return false;
+    }
+
+    public bool Empty()
+    {
+        if (capacity == 0)
+            return true;
+        else return false;
+    }
+}
---
+++ _Test/IPersonContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPersonContainer
+{
+    int capacity { get; set;}
+    int users { get; set; }
+
+    void AddUsers(int numUsers);
+    void RemoveUsers(int numUsers);
+
+    bool Full();
+    bool Empty();
+}
