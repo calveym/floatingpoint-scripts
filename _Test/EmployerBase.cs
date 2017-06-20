c5a761ebed1348f753c908b3de9b7aa1a3c5a6ee
Michael Calvey <mjcalvey2@gmail.com>
2017-06-20 21:50:42 +0100

    EmployerBase

---
+++ _Test/EmployerBase.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using Autelia.Serialization;
+
+public abstract class EmployerBase : BuildingBase
+{
+
+    [Space(10)]
+    [Header("Employer Attributes")]
+    [Space(5)]
+    public int employerRange;
+
+    public bool ApplyForJob(float applicantLandValue, Residential applicantTracker)
+    {
+        float landValueDifference = landValue - applicantLandValue;
+        landValueDifference = Mathf.Abs(landValueDifference);
+        if (landValueDifference < (landValue / 3) && users < capacity && !Serializer.IsLoading)
+        {
+            return AcceptApplication(applicantTracker);
+        }
+        else if (UnityEngine.Random.Range(0, 5) > 3 && !Serializer.IsLoading)
+        {
+            return AcceptApplication(applicantTracker);
+        }
+        else return false;
+    }
+
+    protected virtual bool AcceptApplication(Residential applicantTracker)
+    {
+        return true;
+    }
+
+    protected override void RequestUsers()
+    {
+        
+    }
+}
